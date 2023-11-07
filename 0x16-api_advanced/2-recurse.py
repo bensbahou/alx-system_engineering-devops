83ab21bbb4afcbed215cf999e51e2fa0b68e40d7
bensbahou <younesbensbahou@gmail.com>
2023-11-07 20:51:33 +0100

    2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
-"""Returns the top ten hot posts for a given subreddit"""
-from requests import get
+"""
+Recursive function that queries the Reddit API and returns
+a list containing the titles of all hot articles for a given subreddit.
+If no results are found for the given subreddit,
+the function should return None.
+"""
+
+import requests
 
 
 def recurse(subreddit, hot_list=[], after=""):
-    """Returns the top ten hot posts for a given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
-    response = get(url, headers={"User-Agent": "My-User-Agent"},
-                   allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json().get("data")
-        for child in data.get("children"):
-            hot_list.append(child.get("data").get("title"))
-        after = data.get("after")
-        if after:
+    """
+    Queries the Reddit API and returns
+    a list containing the titles of all hot articles for a given subreddit.
+
+    - If not a valid subreddit, return None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"after": after},
+    )
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            hot_list.append(title)
+        after = req.json().get("data").get("after")
+
+        if after is None:
+            return hot_list
+        else:
             return recurse(subreddit, hot_list, after)
-        return hot_list
     else:
         return None
